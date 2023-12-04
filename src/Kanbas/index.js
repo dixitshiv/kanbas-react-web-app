1a7f26fae5cf91847085abedbfc450d14c1624d4
dixitshiv <145235016+dixitshiv@users.noreply.github.com>
2023-12-03 20:35:16 -0800

    a6 Mongo f23

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,10 +1,10 @@
+import Signin from "../users/signin";
 import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import Modules from "./Courses/Modules";
-
-
+import UserTable from "../users/table";
 function Kanbas() {
   return (
     <div className="d-flex">
@@ -16,6 +16,9 @@ function Kanbas() {
           <Route path="Dashboard" element={<Dashboard />} />
           <Route path="Courses/:courseId/*" element={<Courses />} />
           <Route path="Modules" element={<Modules />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/admin/users" element={<UserTable />} />
+          <Route path="/account/:id" element={<account />} />
         </Routes>
       </div>
     </div>
